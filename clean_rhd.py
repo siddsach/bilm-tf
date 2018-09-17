3a36216274cff845adcf782202fca8c289647a99
siddsach <siddsach@gmail.com>
2018-09-17 11:16:50 -0700

    fixed normalization to work well

--- clean_rhd.py
+++ clean_rhd.py
@@ -1,27 +1,30 @@
 from roamtokenize import WordOnlyTokenizer
-from roamtokenize.regex_strings import *
 import re
+from nltk.tokenize import sent_tokenize
 
-normalization_patterns = {
-        "date_re" : ('', '**DATE**'
-        "number_re" : ('\s[0-9]+\s', '**NUMBER**')
-        "long_ellipses" : ('','**ELLIPSE**'),
-        "new lines" : ('\n'. ' '),
-        "de-ids" : ('[A-Z0-9]{4,}', "**DEIDENTIFIED**"),
-        "special numbers": "[0-9]+|\p+": "**SPECIALNUMBER**",
-        }
+normalization_patterns = [
+        ("long_punctuation_formatting", '(\-{3,}|\.{3,}|\_{3,})',' FORMATTING '),
+        ("de-ids", '(([0-9]+[A-Z]+)+[0-9]*|([A-Z]+[0-9]+)+[A-Z]*)', " DEIDENTIFIED "),
+        ("data_re", r"\*\*DATE\[\d\d/\d\d(/\d\d\d\d)?]", " DATE "),
+        ("initial", r" [A-Z]\.", "INITIAL"),
+        ("number_re", '[0-9]+', 'NUMBER')]
+        #("whitespace", '\s',  ' ')]
 
-things_to_normalize = list(normalization_patterns.keys())
+p_to_test = ['date_re', 'number_re']
+
 
 def normalize(text):
-
-    for pattern in things_to_normalize:
-        re.sub(text, normalization_patterns[pattern][0], normalization_patterns[pattern][1])
-
+    for pattern in normalization_patterns:
+        text = re.sub(pattern[1], pattern[2], text)
     return text
 
+def tokenize(text):
+    text = [sent_tokenize(s) for s in text.split('\n') if s]
+    sentences = [[word_tokenize(sent) for sent in line] for line in text]
+    sentences = '\n'.join(['\n'.join(s) for s in sentences])
+    return sentences
 
 TOKENIZER = WordOnlyTokenizer()
 
-def str_tokenize(text):
+def word_tokenize(text):
     return " ".join(TOKENIZER.tokenize(text))
